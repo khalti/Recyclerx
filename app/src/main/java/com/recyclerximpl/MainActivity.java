cd1592432446a884beafea7cbdf54e2b958c6822
Ishwor Shrestha <ishwor@janakitech.com>
2018-07-13 16:21:39 +0545

    "update"

--- app/src/main/java/com/recyclerximpl/MainActivity.java
+++ app/src/main/java/com/recyclerximpl/MainActivity.java
@@ -4,21 +4,22 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.View;
-import android.widget.ImageView;
+
+import com.recyclerx.utils.EmptyUtil;
+import com.recyclerx.widget.RecyclerX;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import io.reactivex.Observable;
 import io.reactivex.disposables.CompositeDisposable;
 
 public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.rvList)
-    RecyclerView rvList;
+    RecyclerX rvList;
 
     private NameAdapter adapter;
     private CompositeDisposable compositeDisposable;
@@ -31,21 +32,18 @@ public class MainActivity extends AppCompatActivity {
 
         compositeDisposable = new CompositeDisposable();
 
-        fetchList(true);
-//        rvList.setPullToRefreshColor(R.color.colorPrimary, R.color.colorAccent, R.color.colorPrimaryDark);
+        fetchList(false);
+        rvList.setPullToRefreshColor(R.color.colorPrimary, R.color.colorAccent, R.color.colorPrimaryDark);
 
-//        rvList.setOnPullToRefreshListener(() -> new Handler().postDelayed(() -> rvList.setListRefreshing(false), 3000));
-//        rvList.setTryAgainListener(() -> fetchList(true));
+        Observable<Object> pullToRefresh = rvList.setOnPullToRefreshListener();
+        Observable<Object> tryAgain = rvList.setOnTryAgainListener();
 
-//        Observable<Object> pullToRefresh = rvList.setOnPullToRefreshListener();
-//        Observable<Object> tryAgain = rvList.setTryAgainListener();
-
-       /* if (EmptyUtil.isNotNull(pullToRefresh)) {
+        if (EmptyUtil.isNotNull(pullToRefresh)) {
             compositeDisposable.add(pullToRefresh.subscribe(o -> new Handler().postDelayed(() -> rvList.setListRefreshing(false), 3000)));
         }
         if (EmptyUtil.isNotNull(tryAgain)) {
             compositeDisposable.add(tryAgain.subscribe(o -> fetchList(true)));
-        }*/
+        }
     }
 
     private void fetchList(boolean success) {
@@ -69,30 +67,27 @@ public class MainActivity extends AppCompatActivity {
 
         adapter = new NameAdapter(this, names);
 
-        View loadView = getLayoutInflater().inflate(R.layout.custom_view, null);
+        /*View loadView = getLayoutInflater().inflate(R.layout.custom_view, null);
         ((ImageView) loadView.findViewById(R.id.iv)).setImageResource(R.mipmap.cat);
-//        rvList.setCustomLoadingView(loadView);
+        rvList.setCustomLoadingView(loadView);
 
         View errorView = getLayoutInflater().inflate(R.layout.custom_view, null);
         ((ImageView) errorView.findViewById(R.id.iv)).setImageResource(R.drawable.ic_launcher_background);
-//        rvList.setCustomErrorView(errorView);
+        rvList.setCustomErrorView(errorView);*/
 
-//        slLoad.toggleLoading(true);
+        rvList.toggleLoading(true);
         new Handler().postDelayed(() -> {
-//            slLoad.toggleLoading(false);
+            rvList.toggleLoading(false);
             if (success) {
-                LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-//                rvList.setupList(adapter, linearLayoutManager);
-                rvList.setAdapter(adapter);
-                rvList.setLayoutManager(linearLayoutManager);
+                rvList.setupList(adapter, new LinearLayoutManager(this));
 
-                /*Observable<Object> scroll = rvList.setOnScrollListener(10);
+                Observable<Object> scroll = rvList.setOnScrollListener(10);
                 if (EmptyUtil.isNotNull(scroll)) {
                     compositeDisposable.add(scroll.subscribe(o -> fetchMore()));
-                }*/
+                }
 //                rvList.setOnScrollListener(10, this::fetchMore);
             } else {
-//                slLoad.toggleError(true);
+                rvList.toggleError(true);
             }
         }, 3000);
     }
