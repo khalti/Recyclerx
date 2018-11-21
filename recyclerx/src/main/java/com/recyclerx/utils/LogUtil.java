7b46c4987e2cb8bfae18cdcbf3554b7bf3ecb71b
Ishwor Shrestha <ishworisgod@gmail.com>
2018-11-21 11:50:49 +0545

    removed log

--- recyclerx/src/main/java/com/recyclerx/utils/LogUtil.java
+++ recyclerx/src/main/java/com/recyclerx/utils/LogUtil.java
@@ -4,7 +4,7 @@ import android.util.Log;
 
 public class LogUtil {
     // Do logs need to be visible ?
-    private static final boolean DEBUG = true;
+    private static final boolean DEBUG = false;
 
     public static void log(String tag, Object message) {
         if (DEBUG) {
